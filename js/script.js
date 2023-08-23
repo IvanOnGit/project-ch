dd4067014d20ddc25294615977ae5972c5df1583
lazarogabriel <lonofre@goiar.com>
2023-08-22 23:32:33 -0300

    adds reservation functionality

--- js/script.js
+++ js/script.js
@@ -8,17 +8,24 @@ async function renderReservations() {
   try {
     const response = await fetch("./js/reservations.json");
     const reservations = await response.json();
-    
-    console.log({ reservations });
-
     const reservationsContainer = document.getElementById("reservationsContainer");
 
     let reservationElement = "";
     reservations.forEach(reservation => {
       reservationElement +=
       `
-        <div class="pindonga">
-          <strong>${reservation.name}:</strong> ${reservation.room}
+        <div class="reservationCard ${reservation.reserved ? "reservedCard" : ""}">
+          <div id="reservation:${reservation.id}" class="reserverdOverlay">
+            <p>Reserverd</p>
+          </div>
+          <div class="reservationImageContainer"> 
+            <img class="reservationImage" src="./img/${reservation.name}.jpg" alt="Imagen" />
+          </div>
+          <div class="reservationText">
+            <h3>${reservation.name}</h3>
+            <p>Central Park, this metropolis will captivate you with its cultural diversity, world-class dining, and endless entertainment options. Seize this unique opportunity to explore bustling streets, stroll a</p> 
+            <button onclick="reserveRoom(${reservation.id})" class="reserveButton">Reserve</button>      
+          </div>
         </div>
       `;
     });
@@ -28,4 +35,9 @@ async function renderReservations() {
   } catch (error) {
     console.error("Error fetching reservations:", error);
   }
+}
+
+const reserveRoom = (id) => {
+  const reservationElement = document.getElementById(`reservation:${id}`);
+  reservationElement.style.display = "block";
 }
